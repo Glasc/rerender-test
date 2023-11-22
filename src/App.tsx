cfb2a9111db120267b70af33e8e6d63ed1ca3785
Glasc <63518997+Glasc@users.noreply.github.com>
2023-11-22 14:54:29 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Form } from "./Form"
+
+function App() {
+  return (
+    <div className="min-h-screen p-8 bg-[rgb(255,252,240)] text-[rgb(16,15,15)]">
+      <Form />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Form.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react"
+
+function addLeadingZero(digit: number) {
+  // Convert the digit to a string
+  let digitString = digit.toString()
+
+  // Check if the length is 1 (single-digit number)
+  if (digitString.length === 1) {
+    // Add a leading zero
+    digitString = "0" + digitString
+  }
+
+  return digitString
+}
+const getInitialState = (amount: number) => {
+  const initialState: {
+    [key: string]: string
+  } = {}
+  for (let i = 0; i < amount; i++) {
+    initialState[i] = ""
+  }
+  return initialState
+}
+export const Form = () => {
+  const [amount, setAmount] = useState<number>(10)
+  const [inputValue, setInputValue] = useState(getInitialState(amount))
+
+  return (
+    <div>
+      <form>
+        <label className=" font-semibold" htmlFor="amount">
+          Amount:{" "}
+        </label>
+        <input
+          className="border-2 font-bold px-1 border-[rgb(218,216,206)]"
+          type="text"
+          name="amount"
+          id="amount"
+          value={amount}
+          onChange={(e) => {
+            let newAmount = Number(e.target.value)
+            if (isNaN(Number(e.target.value))) {
+              return
+            }
+            newAmount = Math.max(1, newAmount)
+            setAmount(newAmount)
+            setInputValue(getInitialState(newAmount))
+          }}
+        />
+      </form>
+      <form className="flex gap-2 flex-wrap mt-4">
+        {Object.keys(inputValue).map((key) => {
+          return (
+            <div key={key}>
+              <label htmlFor={key}>{addLeadingZero(Number(key) + 1)}: </label>
+              <input
+                className="border-2 px-1 border-[rgb(230,228,217)]"
+                type="text"
+                id={key}
+                name={key}
+                value={inputValue[key as keyof typeof inputValue]}
+                onChange={(e) => {
+                  setInputValue({
+                    ...inputValue,
+                    [key]: e.target.value,
+                  })
+                }}
+              />
+            </div>
+          )
+        })}
+      </form>
+    </div>
+  )
+}
